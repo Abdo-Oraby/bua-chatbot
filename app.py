b2bfe9f991a3bfa75f39582d2940d3f126f08b6d
Abdo-Oraby <labt65349@gmail.com>
2025-09-03 22:03:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def ask():
     return jsonify({"response": bot_response})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
